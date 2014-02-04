156703f3af235b8ee0a74e853cf5641914204692
Plinio Balduino <pbalduino+github@gmail.com>
2014-02-04 16:56:20 -0200

    zipper

--- 09/autores/src/autores/core.clj
+++ 09/autores/src/autores/core.clj
@@ -1,14 +1,37 @@
 (ns autores.core
   (:require [crouton.html   :as html]
-            [clojure.pprint :as pp]))
+            [clojure.pprint :as pp]
+            [clojure.walk   :as walk]))
 
+(defn- read-link [node]
+  (let [link ((node :attrs) :href)]
+    link))
 
-(defn- body
+(defn- trasverse [node]
+  (cond (map? node)
+          (if (= :a (node :tag))
+            (read-link node)
+            (trasverse (node :content)))
+        (vector? node)
+          (map trasverse node)
+        :else node))
+
+(defn- remove-spaces [dom]
+  (filter map? (dom :content)))
+
+(defn- get-body [dom]
+  (first 
+    (filter #(= :body (% :tag)) 
+            (remove-spaces dom))))
+
+(defn- get-section [dom]
+  (first
+    (filter #(= :section (% :tag))
+            (remove-spaces dom))))
+
 (defn -main [& args]
-  (let [content ((html/parse "http://www.casadocodigo.com.br") :content)]
-    (doseq [x content]
-      (println "-------------------")
-      (println x))
-    (println "------------------------------")
-    (println (map map? content))
-    ))
+  (let [section (-> "http://www.casadocodigo.com.br"
+                 html/parse
+                 get-body
+                 get-section)]
+    (println (walk/prewalk trasverse section))))
