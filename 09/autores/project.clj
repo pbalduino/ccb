ae51c38b46a100cfecaf7c26ac7e61f1b085ee4d
Plinio Balduino <pbalduino+github@gmail.com>
2014-02-06 15:49:52 -0200

    limpo

--- 09/autores/project.clj
+++ 09/autores/project.clj
@@ -1,8 +1,4 @@
 (defproject autores "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [enlive "1.1.5"]]
   :main autores.core)
